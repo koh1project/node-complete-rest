b3a2d6dd97dee14dfab369fe8978ef754a803137
Koichi <koichi89592@gmail.com>
2022-05-28 23:24:52 -0700

    Set multer

--- rest_backend/app.js
+++ rest_backend/app.js
@@ -1,15 +1,42 @@
+//@ts-check
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
 
 const feedRoutes = require('./routes/feed');
 
+
 const app = express();
+
+
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'images');
+  },
+  filename: function(req, file, cb) {
+    cb(null, uuidv4())
+  }
+});
+
+/**@param {Express.Multer.File} file @param {multer.FileFilterCallback} cb*/
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' ||
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use(multer({ storage: storage, fileFilter: fileFilter }).single('image'));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
@@ -28,7 +55,9 @@ const errorHandler = (error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
   const message = error.message;
+
   res.status(status).json({ message: message });
+
 };
 
 app.use(errorHandler);
@@ -40,5 +69,5 @@ mongoose.connect('mongodb+srv://node:1234abcd@cluster0.nwkss.mongodb.net/myFirst
   }).catch(err => { console.log(err); });
 
 /**
- * @typedef {(error: Error, req: Express.Request, res: Express.Response, next: import('express').NextFunction) => void} ErrorHandler
+ * @typedef {(error: Error &  {statusCode: number;}, req: Express.Request, res: any, next: import('express').NextFunction) => void} ErrorHandler
  */
--- rest_backend/controllers/feed.js
+++ rest_backend/controllers/feed.js
@@ -23,12 +23,20 @@ exports.createPost = (req, res, next) => {
     throw error;
   }
 
+  if (!req.file) {
+    const error = new Error('No image provided.');
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const imageUrl = req.file.path.replace("\\", "/");
+
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: 'images/test.jpg',
+    imageUrl: imageUrl,
     creator: { name: 'Koichi' },
   });
   post.save().then(result => {
