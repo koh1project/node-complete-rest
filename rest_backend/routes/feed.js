a1142af52c54bea564db656780c760ade2a2d1fa
Koichi <koichi89592@gmail.com>
2022-06-05 16:58:17 -0700

    Add jwt functions

--- rest_backend/routes/feed.js
+++ rest_backend/routes/feed.js
@@ -2,22 +2,23 @@ const express = require('express');
 const { body } = require('express-validator/check');
 
 const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 // POST /feed/post
 router.post(
   '/post',
   [
     body('title')
-      .trim()
-      .isLength({ min: 5 }),
+    .trim()
+    .isLength({ min: 5 }),
     body('content')
-      .trim()
-      .isLength({ min: 5 })
+    .trim()
+    .isLength({ min: 5 })
   ],
   feedController.createPost
 );
@@ -28,11 +29,11 @@ router.put(
   '/post/:postId',
   [
     body('title')
-      .trim()
-      .isLength({ min: 5 }),
+    .trim()
+    .isLength({ min: 5 }),
     body('content')
-      .trim()
-      .isLength({ min: 5 })
+    .trim()
+    .isLength({ min: 5 })
   ],
   feedController.updatePost
 );
