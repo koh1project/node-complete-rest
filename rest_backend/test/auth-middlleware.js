765095b3a2f3c27745cf7ef336c1d329d09d47d0
Koichi <koichi89592@gmail.com>
2022-06-05 22:56:55 -0700

    Used sinon stub

--- rest_backend/test/auth-middlleware.js
+++ rest_backend/test/auth-middlleware.js
@@ -1,5 +1,7 @@
 const expect = require('chai').expect;
 const authMiddleware = require('../middleware/is-auth');
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon');
 
 describe('Auth middleware', () => {
 
@@ -21,4 +23,31 @@ describe('Auth middleware', () => {
     };
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
   });
+
+  it('should yield a userId after decoding the token', () => {
+    const req = {
+      get: (headerName) => {
+        return 'Bearer xyz';
+      }
+    };
+    sinon.stub(jwt, 'verify');
+    jwt.verify.returns({ userId: 'abc' })
+
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property('userId');
+    jwt.verify.restore();
+  });
+
+
+  it('should throw an error if the token cannot be verified', () => {
+    const req = {
+      get: (headerName) => {
+        return 'Bearer xyz';
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+
+
 });
