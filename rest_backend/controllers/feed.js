d149ec8151307600814a4b90e51e1d2373f0ca54
Koichi <koichi89592@gmail.com>
2022-06-05 18:57:56 -0700

    Add logged in check to delete

--- rest_backend/controllers/feed.js
+++ rest_backend/controllers/feed.js
@@ -129,6 +129,13 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized.');
+        error.statusCode = 403;
+        throw error;
+      }
+
       if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
       }
@@ -155,6 +162,11 @@ exports.deletePost = (req, res, next) => {
       if (!post) {
         const error = new Error('Could not find post.');
         error.statusCode = 404;
+        throw error;
+      }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized.');
+        error.statusCode = 403;
         throw error;
       }
       // Check logged in user
