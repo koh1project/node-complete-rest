be6c6397b3e4f2c01ae0c270a3e8000a48d2896e
Koichi <koichi89592@gmail.com>
2022-06-05 22:35:03 -0700

    Add describe

--- rest_backend/test/auth-middlleware.js
+++ rest_backend/test/auth-middlleware.js
@@ -1,11 +1,24 @@
 const expect = require('chai').expect;
 const authMiddleware = require('../middleware/is-auth');
 
-it('should throw an error if no authorization header is present', () => {
-  const req = {
-    get: (headerName) => {
-      return null;
-    }
-  };
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not authenticated');
+describe('Auth middleware', () => {
+
+  it('should throw an error if no authorization header is present', () => {
+    const req = {
+      get: (headerName) => {
+        return null;
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not authenticated');
+  });
+
+
+  it('should throw an error if the authorization header is only one string', () => {
+    const req = {
+      get: (headerName) => {
+        return 'xyz';
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
 });
