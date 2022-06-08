98e5a7969a9a8e4f9b6f913422c928472dcd9eda
Koichi <koichi89592@gmail.com>
2022-06-07 22:25:58 -0700

    Add other tests

---
+++ rest_backend/test/feed-controller.js
@@ -0,0 +1,64 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const mongoose = require('mongoose');
+
+const User = require('../models/user');
+const FeedController = require('../controllers/feed');
+
+describe('Feed Controller', function() {
+  before(function(done) {
+    mongoose.connect('mongodb+srv://node:1234abcd@cluster0.nwkss.mongodb.net/myFirstDatabase')
+      .then(result => {
+        const user = new User({
+          email: 'test@test.com',
+          password: 'tester',
+          name: 'Test',
+          posts: [],
+          _id: '5c0f66b979af55031b34728b'
+        });
+        return user.save();
+      })
+      .then(() => {
+        done();
+      });
+  });
+
+  beforeEach(function() {});
+
+  afterEach(function() {});
+
+  it('should add a created post to the posts of the creator', function(done) {
+    const req = {
+      body: {
+        title: 'Test Post',
+        content: 'A Test Post'
+      },
+      file: {
+        path: 'abc'
+      },
+      userId: '5c0f66b979af55031b34728b'
+    };
+    const res = {
+      status: function() {
+        return this;
+      },
+      json: function() {}
+    };
+
+    FeedController.createPost(req, res, () => {}).then(savedUser => {
+      // expect(savedUser).to.have.property('posts');
+      // expect(savedUser.posts).to.have.length(1);
+      done();
+    });
+  });
+
+  after(function(done) {
+    User.deleteMany({})
+      .then(() => {
+        return mongoose.disconnect();
+      })
+      .then(() => {
+        done();
+      });
+  });
+});
